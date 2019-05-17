dedb2bea416f35c04b106aef8d336bc9f0fe865d
ThomasSarlin <thomas.sarlin@gmail.com>
2019-05-17 15:20:58 +0200

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import Intro1 from './components/Intro1'
+import DisplayResult from './components/DisplayResult'
 import Intro2 from './components/Intro2'
 import { BrowserView, MobileView, isBrowser, isMobile } from "react-device-detect";
 import Controller from './components/Controller';
 var footer={
-	fontSize:'10px'
+	fontSize:'11px'
 }
 export default class App extends Component {
 	constructor(){
 		super();
 		this.state={
-			case:1
+			case:4
 		}
 	}
 	inc(){
 		this.setState({
 			case:this.state.case+1
+		})
+	}
+	showresult(){
+		this.setState({
+			case:4
 		})
 	}
 	render() {
@@ -28,6 +34,7 @@ export default class App extends Component {
 						{this.state.case===1?<Intro1 incFunc={this.inc.bind(this)} />:""}
 						{this.state.case===2?<Intro2 incFunc={this.inc.bind(this)} />:""}
 						{this.state.case===3?<Controller database={this.props.database}/>:""}
+						{this.state.case===4?<DisplayResult database={this.props.database}/>:""}
 						<footer style={footer}>Made by Thomas Sarlin, thomas@greyleaf.se</footer>
 					</header>
 				</div>
